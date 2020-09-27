68f9b0a98ed26bc0ca6f8b071d985868620bdfe8
AaryanShaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-09-27 13:52:59 +0530

    Create prob1.java

---
+++ JAVA/prob1.java
@@ -0,0 +1,18 @@
+class Rectangle{
+	private int length;
+	private int breadth;
+	Rectangle(int x, int y){
+		length = x;
+		breadth = y;
+	}
+	
+	int area(){
+		return length*breadth;
+	}
+}
+public class prob1{
+	public static void main(String[] args) {
+		Rectangle rec = new Rectangle(30,20);
+		System.out.println("Area of Rectangle is "+rec.area());
+	}
+}
