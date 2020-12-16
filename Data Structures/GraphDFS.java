8db5a4eb3cd8611cd26e1772e1bc301e763fb226
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-12-16 17:46:47 +0530

    Add files via upload

---
+++ Data Structures/GraphDFS.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class GraphDFS {
+  private LinkedList<Integer> adjLists[];
+  private boolean visited[];
+
+  GraphDFS(int vertices) {
+    adjLists = new LinkedList[vertices];
+    visited = new boolean[vertices];
+
+    for (int i = 0; i < vertices; i++)
+      adjLists[i] = new LinkedList<Integer>();
+  }
+
+  void addEdge(int src, int dest) {
+    adjLists[src].add(dest);
+  }
+
+  void DFS(int vertex) {
+    visited[vertex] = true;
+    System.out.print(vertex + " ");
+
+    Iterator<Integer> ite = adjLists[vertex].listIterator();
+    while (ite.hasNext()) {
+      int adj = ite.next();
+      if (!visited[adj])
+        DFS(adj);
+    }
+  }
+
+  public static void main(String args[]) {
+    GraphDFS g = new GraphDFS(4);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+
+    System.out.println("Following is Depth First Traversal");
+
+    g.DFS(2);
+  }
+}
