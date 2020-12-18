b1efeb62e6222ebd5e6dada3b8114c3e0d87ac48
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-12-18 10:27:39 +0530

    Update read.py

--- Maths/read.py
+++ Maths/read.py
@@ -26,3 +26,43 @@ print(mydata2.head())
 
 # Gives Summary about the dataset
 print(mydata2.describe())
+
+#all the objects of the dataset
+print(mydata2.columns[:])
+
+#all the objects of the dataset -1
+print(mydata2.columns[:-1])
+
+#will display all the values and check if they contain 0 or not 
+print(mydata2[mydata2.columns[:-1]]==0)
+
+#this will display true and false column with the help of this we can extract the data required 
+print((mydata2[mydata2.columns[:-1]]==0).any(axis=1))
+
+#this will extract rows which does contain 0 
+mydata3 = mydata2.loc[(mydata2[mydata2.columns[:-1]]==0).any(axis=1)]
+print(mydata3)
+
+#this will extract rows which does not contain 0
+mydata4 = mydata2.loc[~(mydata2[mydata2.columns[:-1]]==0).any(axis=1)]
+print(mydata4)
+print(mydata4.describe())
+
+#this will give the mean of all the values which are singlarly grouped
+print(mydata4.groupby("Outcome").mean())
+
+#to calculate different aggregation(averages) for difffernt data
+print(mydata4.groupby("Outcome").agg({"Glucose":"mean","BMI":"median","Age":"sum"}))
+
+#how to seperate dataframe
+print(mydata4["Outcome"]==1)
+print(mydata4["Outcome"]==0)
+
+diabiatic = mydata4.loc[mydata4["Outcome"]==1]
+nonDiabiatic = mydata4.loc[mydata4["Outcome"]==0]
+#how many columns are there an rows are there
+print(diabiatic.shape,nonDiabiatic.shape)
+
+#export data
+diabiatic.to_csv("diabiatic.csv")
+nonDiabiatic.to_csv("nonDiabiatic.csv")
